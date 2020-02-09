abfdab04171facfe30b5e0c70b99772bd75a1692
l3uddz <l3uddz@gmail.com>
2020-02-09 01:31:26 +0000

    add function to validate tvdb ids

---
+++ utils/media/tvdb.go
@@ -0,0 +1,28 @@
+package media
+
+import (
+	"github.com/l3uddz/mediarr/logger"
+	"github.com/l3uddz/mediarr/utils/web"
+)
+
+var (
+	log = logger.GetLogger("media_utils")
+)
+
+func ValidateTvdbId(tvdbId string) bool {
+	// send request
+	resp, err := web.GetResponse(web.GET, "https://www.thetvdb.com/dereferrer/series/"+tvdbId, 30)
+	if err != nil {
+		log.WithError(err).Tracef("Failed retrieving tvdb details for: %q", tvdbId)
+		return false
+	}
+	defer resp.Response().Body.Close()
+
+	// validate response
+	if resp.Response().StatusCode != 200 {
+		log.Tracef("failed retrieving valid tvdb details for %q: %s", tvdbId, resp.Response().Status)
+		return false
+	}
+
+	return true
+}
