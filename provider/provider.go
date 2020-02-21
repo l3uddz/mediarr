2855f58f750f3f2cb0b0a4638b2abe6862be3211
l3uddz <l3uddz@gmail.com>
2020-02-21 08:43:46 +0000

    web: increase max  timeout retries to 6, 500ms min/10s max

--- provider/provider.go
+++ provider/provider.go
@@ -11,12 +11,12 @@ import (
 var (
 	providerDefaultTimeout = 30
 	providerDefaultRetry   = web.Retry{
-		MaxAttempts:          5,
+		MaxAttempts:          6,
 		RetryableStatusCodes: []int{},
 		Backoff: backoff.Backoff{
 			Jitter: true,
-			Min:    1 * time.Second,
-			Max:    5 * time.Second,
+			Min:    500 * time.Millisecond,
+			Max:    10 * time.Second,
 		},
 	}
 )
